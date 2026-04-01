8e0dff8834f6bba1b26bf8a4bbf4d8c028c348a3
75HardDevOps <devops@75hard.io>
2026-04-01 12:22:53 +0530

    feat: dsl setup - config.json driven pipeline generation

---
+++ dsl/docker-pipeline.groovy
@@ -0,0 +1,44 @@
+import groovy.json.JsonSlurper
+
+def configFile = readFileFromWorkspace('applications/config.json')
+def config     = new JsonSlurper().parseText(configFile)
+
+config.applications.each { app ->
+
+    pipelineJob("${app.name}-docker-pipeline") {
+
+        description("Docker CI/CD pipeline for ${app.name}")
+
+        definition {
+            cps {
+                script("""
+@Library('jenkins') _
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Clone') {
+            steps {
+                cloneRepo('${app.repo}', '${app.branch}')
+            }
+        }
+        stage('Docker Build') {
+            steps {
+                dockerBuild('${app.name}')
+            }
+        }
+        stage('Trivy Scan') {
+            steps {
+                trivyScan('${app.name}')
+            }
+        }
+    }
+}
+""")
+                sandbox(true)
+            }
+        }
+    }
+
+}
